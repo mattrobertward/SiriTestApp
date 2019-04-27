eda4feec61d0d2cf9316c8ead767d19f51f1a2d2
Matt Ward <mattward@MacBook-Pro.local>
2019-04-27 08:37:17 -0700

    Initial project creation

---
+++ SiriTestApp/SiriTestApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SiriTestApp
+//
+//  Created by Matt Ward on 4/27/19.
+//  Copyright © 2019 mattrobertward. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
